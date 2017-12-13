826d0ed4658b1b86b96b18cc45df1cacb85b8fbe
Derek Lemon <derlemon@gmail.com>
2017-12-13 16:22:00 -0700

    Saving Work

--- prometheus/metrics/docker/uptime.go
+++ prometheus/metrics/docker/uptime.go
@@ -1,31 +1,45 @@
 package docker
 
 import (
+	"os"
+
 	"github.com/docker/docker/api/types"
-	"github.com/drekle/go/prometheus/metrics"
+	"github.com/drekle/go/prometheus/metrics/api"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 type uptime struct {
-	container *types.Container
-	gauge     metrics.Metric
+	container types.Container
+	gauge     prometheus.Gauge
 }
 
 func (metric uptime) Tick() {
-	if metric.container.Status == types.Healthy {
+	if metric.container.State == "running" {
 		metric.gauge.Set(1)
 	} else {
 		metric.gauge.Set(0)
 	}
 }
 
-func UptimeMetric(container *types.Container) metrics.TickMetric {
+func (metric uptime) GetMetric() api.Metric {
+	return metric.gauge
+}
+
+func (metric uptime) GetUUID() string {
+	hostname, _ := os.Hostname()
+	id := hostname + "_" + metric.container.ID
+	return id
+}
+
+func UptimeMetric(container types.Container) api.TickMetric {
+	hostname, _ := os.Hostname()
+	name := hostname + "_" + container.Names[0][1:]
 	return &uptime{
 		container: container,
 		gauge: prometheus.NewGauge(
 			prometheus.GaugeOpts{
-				Name: container.ID,
-				Help: "Uptime for " + container.ID,
+				Name: name,
+				Help: "Uptime for " + name,
 			},
 		),
 	}
