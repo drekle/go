fb50e5f13784b597409d18a7c9a4d44441248793
Derek Lemon <derlemon@gmail.com>
2017-12-13 13:58:10 -0700

    Temp Check Ins

--- prometheus/metrics/docker/uptime.go
+++ prometheus/metrics/docker/uptime.go
@@ -1,5 +1,32 @@
 package docker
 
 import (
-	_ "github.com/drekle/go/prometheus/metrics"
+	"github.com/docker/docker/api/types"
+	"github.com/drekle/go/prometheus/metrics"
+	"github.com/prometheus/client_golang/prometheus"
 )
+
+type uptime struct {
+	container *types.Container
+	gauge     metrics.Metric
+}
+
+func (metric uptime) Tick() {
+	if metric.container.Status == types.Healthy {
+		metric.gauge.Set(1)
+	} else {
+		metric.gauge.Set(0)
+	}
+}
+
+func UptimeMetric(container *types.Container) metrics.TickMetric {
+	return &uptime{
+		container: container,
+		gauge: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Name: container.ID,
+				Help: "Uptime for " + container.ID,
+			},
+		),
+	}
+}
